38f3b7c9ab6c4d2c2304d2633ab39c41f06d5ac6
CAV <cavpollo@gmail.com>
2015-12-16 12:19:51 -0600

    Spanish (ES) Support

--- test/iso-i18n-countries.js
+++ test/iso-i18n-countries.js
@@ -182,6 +182,19 @@ describe("i18n for iso 3166-1", function () {
 			});
 		});
 	});
+	describe("es", function () {
+		var lang = "es";
+		it("complete (to less)", function() {
+			Object.keys(i18niso.getAlpha2Codes()).forEach(function(code) {
+				assert.notEqual(i18niso.getName(code, lang), undefined, "missing entry for " + code);
+			});
+		});
+		it("complete (too much)", function() {
+			Object.keys(i18niso.getNames(lang)).forEach(function(code) {
+				assert.notEqual(i18niso.getAlpha2Codes()[code], -1, "entry for " + code + " is too much");
+			});
+		});
+	});
 	describe("unsupported language", function () {
 		var lang = "unsupported";
 		it("get name", function () {
