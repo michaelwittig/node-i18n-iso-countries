855517c3c5428569451f850656cc1a22db16c9e6
Hans Otto Wirtz <hansottowirtz@gmail.com>
2020-11-21 13:26:12 +0100

    Fix lang-template.d.ts

--- src/lang-template.d.ts
+++ src/lang-template.d.ts
@@ -1,7 +1,6 @@
-export const locale: string;
-export const countries: {
-  [alpha2Key: string]: string[] | string
-};
-export default {
-  locale, countries
-}
+import { LocaleData } from "../lib";
+
+export const locale: LocaleData["locale"];
+export const countries: LocaleData["countries"];
+declare const lang: LocaleData;
+export default lang;
