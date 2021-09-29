945b3ce9d531ae999212560ddbc2e8d57300d7c1
Omar Diab <me@omardiab.com>
2021-09-29 16:13:56 +0900

    add numeric code types, make types more precise

--- index.d.ts
+++ index.d.ts
@@ -4,9 +4,7 @@ export interface GetNameOptions {
 
 export type CountryName<T extends GetNameOptions> = T extends { select: 'all' } ? string[] : string
 
-export type LocalizedCountryNames<T extends GetNameOptions> = {
-  [alpha2Key: string]: CountryName<T>
-};
+export type LocalizedCountryNames<T extends GetNameOptions> = Record<Alpha2Code,  CountryName<T>;
 
 export type LocaleData = {
   locale: string,
@@ -18,36 +16,33 @@ export type LocaleData = {
 
 
 export function registerLocale(localeData: LocaleData): void;
-export function alpha2ToAlpha3(alpha2: string | Alpha2Code): string;
-export function alpha2ToNumeric(alpha2: string | Alpha2Code): string;
-export function alpha3ToAlpha2(alpha3: string | Alpha3Code): string;
-export function alpha3ToNumeric(alpha3: string | Alpha3Code): string;
-export function numericToAlpha2(numeric: number | string): string;
-export function numericToAlpha3(numeric: number | string): string;
+export function alpha2ToAlpha3<Input extends string>(alpha2: Input): Lowercase<Input> extends Lowercase<Alpha2Code> ? Alpha3Code : Alpha3Code | undefined;
+export function alpha2ToNumeric<Input extends string>(alpha2: Input): Lowercase<Input> extends Lowercase<Alpha2Code> ? StringNumericCode : StringNumericCode | undefined;
+export function alpha3ToAlpha2<Input extends string>(alpha3: Input): Lowercase<Input> extends Lowercase<Alpha3Code> ? Alpha2Code : Alpha2Code | undefined;
+export function alpha3ToNumeric<Input extends string>(alpha3: Input): Lowercase<Input> extends Lowercase<Alpha3Code> ? StringNumericCode : StringNumericCode | undefined;
+export function numericToAlpha2<Input extends string | number>(numeric: Input): Input extends NumericCode ? Alpha2Code : Alpha2Code | undefined;
+export function numericToAlpha3<Input extends string | number>(numeric: Input): Input extends NumericCode ? Alpha3Code : Alpha3Code | undefined;
 
 /**
  * Returns object map where key is alpha 2 code and value is alpha 3 code
  */
-export function getAlpha2Codes(): { [alpha2Key: string]: string };
+export function getAlpha2Codes(): Record<Alpha2Code, Alpha3Code>;
 
 /**
  * Returns object map where key is alpha 3 code and value is alpha 2 code
  */
-export function getAlpha3Codes(): { [alpha3Key: string]: string };
+export function getAlpha3Codes(): Record<Alpha3Code, Alpha2Code>;
 
 /**
  * Returns object map where key is numeric code and value is alpha 2 code
  */
-export function getNumericCodes(): { [numericKey: string]: string };
+export function getNumericCodes(): Record<StringNumericCode, Alpha2Code>;
 
 /**
  * @param countryCode  Alpha2 or Alpha3 or Numeric
  * @param lang        ISO 639-1 format string
  */
-export function getName(
-  countryCode: string | number | Alpha2Code | Alpha3Code,
-  lang: string
-): string;
+export function getName(countryCode: string | number, lang: string): string | undefined;
 
 /**
  * @param countryCode  Alpha2 or Alpha3 or Numeric
@@ -55,15 +50,15 @@ export function getName(
  * @param options     Optional config of getName
  */
 export function getName<T extends GetNameOptions>(
-  countryCode: string | number | Alpha2Code | Alpha3Code,
+  countryCode: string | number,
   lang: string,
   options: T
-): CountryName<T>;
+): CountryName<T> | undefined;
 
 /**
  * @param lang    ISO 639-1 format string
  */
-export function getNames(lang: string): LocalizedCountryNames<{ select: 'official' }>;
+export function getNames(lang: string): LocalizedCountryNames<{ select: 'official' }> | {};
 
 /**
  * @param lang      ISO 639-1 format string
@@ -72,21 +67,522 @@ export function getNames(lang: string): LocalizedCountryNames<{ select: 'officia
 export function getNames<T extends GetNameOptions>(
   lang: string,
   options: T
-): LocalizedCountryNames<T>;
+): LocalizedCountryNames<T> | {};
 
-export function getAlpha2Code(name: string, lang: string): string;
-export function getSimpleAlpha2Code(name: string, lang: string): string;
-export function getAlpha3Code(name: string, lang: string): string;
-export function getSimpleAlpha3Code(name: string, lang: string): string;
+export function getAlpha2Code(name: string, lang: string): Alpha2Code | undefined;
+export function getSimpleAlpha2Code(name: string, lang: string): Alpha2Code | undefined;
+export function getAlpha3Code(name: string, lang: string): Alpha3Code | undefined;
+export function getSimpleAlpha3Code(name: string, lang: string): Alpha3Code | undefined;
 export function langs(): string[];
-export function toAlpha3(
-  alpha2orNumeric: number | string | Alpha2Code
-): string;
-export function toAlpha2(
-  alpha3orNumeric: number | string | Alpha3Code
-): string;
-export function isValid(alpha2orAlpha3orNumeric: string | number): boolean;
+export function toAlpha3<Input extends string | number>(
+  alpha2orNumeric: Input
+): Input extends Alpha2Code | NumericCode ? Alpha3Code : (Alpha3Code| undefined);
+export function toAlpha2<Input extends string | number>(
+  alpha3orNumeric: Input
+): Input extends (Alpha3Code | NumericCode) ? Alpha2Code : (Alpha2Code | undefined);
+export function isValid(alpha2orAlpha3orNumeric: string | number): alpha2orAlpha3orNumeric is NumericCode | Alpha2Code | Alpha3Code;
 
+export type StringNumericCode =
+  | "004"
+  | "008"
+  | "010"
+  | "012"
+  | "016"
+  | "020"
+  | "024"
+  | "028"
+  | "031"
+  | "032"
+  | "036"
+  | "040"
+  | "044"
+  | "048"
+  | "050"
+  | "051"
+  | "052"
+  | "056"
+  | "060"
+  | "064"
+  | "068"
+  | "070"
+  | "072"
+  | "074"
+  | "076"
+  | "084"
+  | "086"
+  | "090"
+  | "092"
+  | "096"
+  | "100"
+  | "104"
+  | "108"
+  | "112"
+  | "116"
+  | "120"
+  | "124"
+  | "132"
+  | "136"
+  | "140"
+  | "144"
+  | "148"
+  | "152"
+  | "156"
+  | "158"
+  | "162"
+  | "166"
+  | "170"
+  | "174"
+  | "175"
+  | "178"
+  | "180"
+  | "184"
+  | "188"
+  | "191"
+  | "192"
+  | "196"
+  | "203"
+  | "204"
+  | "208"
+  | "212"
+  | "214"
+  | "218"
+  | "222"
+  | "226"
+  | "231"
+  | "232"
+  | "233"
+  | "234"
+  | "238"
+  | "239"
+  | "242"
+  | "246"
+  | "248"
+  | "250"
+  | "254"
+  | "258"
+  | "260"
+  | "262"
+  | "266"
+  | "268"
+  | "270"
+  | "275"
+  | "276"
+  | "288"
+  | "292"
+  | "296"
+  | "300"
+  | "304"
+  | "308"
+  | "312"
+  | "316"
+  | "320"
+  | "324"
+  | "328"
+  | "332"
+  | "334"
+  | "336"
+  | "340"
+  | "344"
+  | "348"
+  | "352"
+  | "356"
+  | "360"
+  | "364"
+  | "368"
+  | "372"
+  | "376"
+  | "380"
+  | "384"
+  | "388"
+  | "392"
+  | "398"
+  | "400"
+  | "404"
+  | "408"
+  | "410"
+  | "414"
+  | "417"
+  | "418"
+  | "422"
+  | "426"
+  | "428"
+  | "430"
+  | "434"
+  | "438"
+  | "440"
+  | "442"
+  | "446"
+  | "450"
+  | "454"
+  | "458"
+  | "462"
+  | "466"
+  | "470"
+  | "474"
+  | "478"
+  | "480"
+  | "484"
+  | "492"
+  | "496"
+  | "498"
+  | "499"
+  | "500"
+  | "504"
+  | "508"
+  | "512"
+  | "516"
+  | "520"
+  | "524"
+  | "528"
+  | "531"
+  | "533"
+  | "534"
+  | "535"
+  | "540"
+  | "548"
+  | "554"
+  | "558"
+  | "562"
+  | "566"
+  | "570"
+  | "574"
+  | "578"
+  | "580"
+  | "581"
+  | "583"
+  | "584"
+  | "585"
+  | "586"
+  | "591"
+  | "598"
+  | "600"
+  | "604"
+  | "608"
+  | "612"
+  | "616"
+  | "620"
+  | "624"
+  | "626"
+  | "630"
+  | "634"
+  | "638"
+  | "642"
+  | "643"
+  | "646"
+  | "652"
+  | "654"
+  | "659"
+  | "660"
+  | "662"
+  | "663"
+  | "666"
+  | "670"
+  | "674"
+  | "678"
+  | "682"
+  | "686"
+  | "688"
+  | "690"
+  | "694"
+  | "702"
+  | "703"
+  | "704"
+  | "705"
+  | "706"
+  | "710"
+  | "716"
+  | "724"
+  | "728"
+  | "729"
+  | "732"
+  | "740"
+  | "744"
+  | "748"
+  | "752"
+  | "756"
+  | "760"
+  | "762"
+  | "764"
+  | "768"
+  | "772"
+  | "776"
+  | "780"
+  | "784"
+  | "788"
+  | "792"
+  | "795"
+  | "796"
+  | "798"
+  | "800"
+  | "804"
+  | "807"
+  | "818"
+  | "826"
+  | "831"
+  | "832"
+  | "833"
+  | "834"
+  | "840"
+  | "850"
+  | "854"
+  | "858"
+  | "860"
+  | "862"
+  | "876"
+  | "882"
+  | "887"
+  | "894";
+export type NumberNumericCode =
+  | 4
+  | 8
+  | 10
+  | 12
+  | 16
+  | 20
+  | 24
+  | 28
+  | 31
+  | 32
+  | 36
+  | 40
+  | 44
+  | 48
+  | 50
+  | 51
+  | 52
+  | 56
+  | 60
+  | 64
+  | 68
+  | 70
+  | 72
+  | 74
+  | 76
+  | 84
+  | 86
+  | 90
+  | 92
+  | 96
+  | 100
+  | 104
+  | 108
+  | 112
+  | 116
+  | 120
+  | 124
+  | 132
+  | 136
+  | 140
+  | 144
+  | 148
+  | 152
+  | 156
+  | 158
+  | 162
+  | 166
+  | 170
+  | 174
+  | 175
+  | 178
+  | 180
+  | 184
+  | 188
+  | 191
+  | 192
+  | 196
+  | 203
+  | 204
+  | 208
+  | 212
+  | 214
+  | 218
+  | 222
+  | 226
+  | 231
+  | 232
+  | 233
+  | 234
+  | 238
+  | 239
+  | 242
+  | 246
+  | 248
+  | 250
+  | 254
+  | 258
+  | 260
+  | 262
+  | 266
+  | 268
+  | 270
+  | 275
+  | 276
+  | 288
+  | 292
+  | 296
+  | 300
+  | 304
+  | 308
+  | 312
+  | 316
+  | 320
+  | 324
+  | 328
+  | 332
+  | 334
+  | 336
+  | 340
+  | 344
+  | 348
+  | 352
+  | 356
+  | 360
+  | 364
+  | 368
+  | 372
+  | 376
+  | 380
+  | 384
+  | 388
+  | 392
+  | 398
+  | 400
+  | 404
+  | 408
+  | 410
+  | 414
+  | 417
+  | 418
+  | 422
+  | 426
+  | 428
+  | 430
+  | 434
+  | 438
+  | 440
+  | 442
+  | 446
+  | 450
+  | 454
+  | 458
+  | 462
+  | 466
+  | 470
+  | 474
+  | 478
+  | 480
+  | 484
+  | 492
+  | 496
+  | 498
+  | 499
+  | 500
+  | 504
+  | 508
+  | 512
+  | 516
+  | 520
+  | 524
+  | 528
+  | 531
+  | 533
+  | 534
+  | 535
+  | 540
+  | 548
+  | 554
+  | 558
+  | 562
+  | 566
+  | 570
+  | 574
+  | 578
+  | 580
+  | 581
+  | 583
+  | 584
+  | 585
+  | 586
+  | 591
+  | 598
+  | 600
+  | 604
+  | 608
+  | 612
+  | 616
+  | 620
+  | 624
+  | 626
+  | 630
+  | 634
+  | 638
+  | 642
+  | 643
+  | 646
+  | 652
+  | 654
+  | 659
+  | 660
+  | 662
+  | 663
+  | 666
+  | 670
+  | 674
+  | 678
+  | 682
+  | 686
+  | 688
+  | 690
+  | 694
+  | 702
+  | 703
+  | 704
+  | 705
+  | 706
+  | 710
+  | 716
+  | 724
+  | 728
+  | 729
+  | 732
+  | 740
+  | 744
+  | 748
+  | 752
+  | 756
+  | 760
+  | 762
+  | 764
+  | 768
+  | 772
+  | 776
+  | 780
+  | 784
+  | 788
+  | 792
+  | 795
+  | 796
+  | 798
+  | 800
+  | 804
+  | 807
+  | 818
+  | 826
+  | 831
+  | 832
+  | 833
+  | 834
+  | 840
+  | 850
+  | 854
+  | 858
+  | 860
+  | 862
+  | 876
+  | 882
+  | 887
+  | 894;
+export type NumericCode = StringNumericCode | NumberNumericCode;
 
 export type Alpha2Code =
   | "AF"
